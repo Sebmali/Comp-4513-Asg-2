91348c2400d8f8e48d55c122d5aaae2b902ad4da
Sebamalicz <smali211@mtroyal.ca>
2025-03-27 21:16:09 -0600

    Not Found Route Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { supabase } from './Supabase/supabaseClient'
 import LoginPage from "./components/LoginPage/LoginPage.jsx"
 import './App.css'
-
-// Existing views
+import NotFound from './components/NotFound'
 import GalleryView from './components/GalleyView/GalleryView'
 import ArtistView from './components/ArtistView/ArtistView'
-
-// Import your new GenreView
 import GenreView from './components/GenreView/GenreView'
 import NavBar from './components/NavBar'
 import FooterBar from './components/FooterBar'
@@ -149,6 +146,7 @@ function App() {
             <Route path="/galleries" element={<GalleryView galleries={galleries} paintings={paintings} artists={artists} favourites={favourites} setFavourites={setFavourites}/>} />
             <Route path="/artists" element={<ArtistView artists={artists} paintings={paintings} favourites={favourites} setFavourites={setFavourites}/>} />
             <Route path="/genres" element={<GenreView genres={genres} paintings={paintings} artists={artists} paintinggenres={paintinggenres} favourites={favourites} eras={eras} setFavourites={setFavourites}/>} />
+            <Route path="*" element={<NotFound />} /> {/* Potentially want to redirect users to home page */}
           </Routes>
         </div>
         <FooterBar/>
